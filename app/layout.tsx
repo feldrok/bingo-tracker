36a019cd0f0163d9f23be52371bc75ddc9ab37ed
Matias Mora Valero <matias@moravalero.cl>
2024-10-19 20:08:26 -0300

    feat: analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,7 @@ export default function RootLayout({
         <main className="min-h-screen bg-gradient-to-b bg-background text-foreground p-4 md:p-8">
           {children}
         </main>
+        <Analytics />
       </body>
     </html>
   );
