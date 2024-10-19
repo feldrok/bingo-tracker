8da6038c6b6527b8ee2ef5f44d68c331c205f531
Matias Mora Valero <matias@moravalero.cl>
2024-10-19 19:22:21 -0300

    feat: improvements

--- components/drawn-numbers.tsx
+++ components/drawn-numbers.tsx
@@ -17,10 +17,6 @@ export function DrawnNumbers({
 }) {
   const [numbers, setNumbers] = useState<DrawnNumber[]>(drawnNumbers);
 
-  const drawnNumbersSet = useMemo(() => {
-    return new Set(numbers.map((item) => item.number));
-  }, [numbers]);
-
   useEffect(() => {
     const channel = supabase
       .channel("realtime numbers")
@@ -56,25 +52,36 @@ export function DrawnNumbers({
   }, [drawnNumbers]);
 
   return (
-    <div className="bg-white bg-opacity-20 rounded-lg p-4 w-full">
-      <h2 className="text-2xl md:text-3xl font-bold mb-1">Números sorteados</h2>
-      <p className="text-muted-foreground mb-4">{numbers.length} de 75</p>
+    <>
+      <div className="text-center mb-8 w-full">
+        <p className="text-2xl md:text-4xl">Último número</p>
+        <p className="text-6xl md:text-[14rem] font-bold mb-4 text-primary">
+          {numbers[0].letter}-{numbers[0].number}
+        </p>
+      </div>
 
-      <h3 className="text-xl md:text-2xl font-bold mb-4">{gameName}</h3>
-      {numbers.length > 0 ? (
-        <div className="flex flex-wrap gap-2">
-          {numbers.map((item) => (
-            <div
-              key={item.id}
-              className="bg-primary text-primary-foreground rounded-lg p-2 text-center font-bold whitespace-nowrap w-fit"
-            >
-              {item.letter}-{item.number}
-            </div>
-          ))}
-        </div>
-      ) : (
-        <div className="text-center">No hay números sorteados aún.</div>
-      )}
-    </div>
+      <div className="bg-white bg-opacity-20 rounded-lg p-4 w-full">
+        <h2 className="text-2xl md:text-3xl font-bold mb-1">
+          Números sorteados
+        </h2>
+        <p className="text-muted-foreground mb-4">{numbers.length} de 75</p>
+
+        <h3 className="text-xl md:text-2xl font-bold mb-4">{gameName}</h3>
+        {numbers.length > 0 ? (
+          <div className="flex flex-wrap gap-2">
+            {numbers.map((item) => (
+              <div
+                key={item.id}
+                className="bg-primary text-primary-foreground rounded-lg p-2 text-center font-bold whitespace-nowrap w-fit"
+              >
+                {item.letter}-{item.number}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="text-center">No hay números sorteados aún.</div>
+        )}
+      </div>
+    </>
   );
 }
--- components/realtime-bingo.tsx
+++ components/realtime-bingo.tsx
@@ -3,83 +3,77 @@
 import type { getActiveGame } from "@/utils/bingoUtils";
 import { supabase } from "@/utils/supabase/client";
 import { useEffect, useState } from "react";
-import { LastNumber } from "./last-number";
 import { type DrawnNumber, RealtimeNumbers } from "./bingo";
 import { DrawnNumbers } from "./drawn-numbers";
 
 type ActiveGame = Awaited<ReturnType<typeof getActiveGame>>;
 
 export function RealtimeBingo({
-	serverActiveGame,
-	drawnNumbers: initialDrawnNumbers,
-	gameName,
+  serverActiveGame,
+  drawnNumbers: initialDrawnNumbers,
 }: {
-	serverActiveGame: ActiveGame;
-	drawnNumbers: DrawnNumber[];
-	gameName: string;
+  serverActiveGame: ActiveGame;
+  drawnNumbers: DrawnNumber[];
 }) {
-	const [activeGame, setActiveGame] = useState<ActiveGame>(serverActiveGame);
-	const [drawnNumbers, setDrawnNumbers] =
-		useState<DrawnNumber[]>(initialDrawnNumbers);
+  const [activeGame, setActiveGame] = useState<ActiveGame>(serverActiveGame);
+  const [drawnNumbers, setDrawnNumbers] =
+    useState<DrawnNumber[]>(initialDrawnNumbers);
 
-	useEffect(() => {
-		const channel = supabase
-			.channel("realtime game")
-			.on(
-				"postgres_changes",
-				{
-					event: "*",
-					schema: "public",
-					table: "games",
-				},
-				async (payload) => {
-					const event = payload.new as {
-						created_at: Date;
-						event_id: number;
-						id: number;
-						is_active: boolean;
-						name: string;
-					};
+  useEffect(() => {
+    const channel = supabase
+      .channel("realtime game")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "games",
+        },
+        async (payload) => {
+          const event = payload.new as {
+            created_at: Date;
+            event_id: number;
+            id: number;
+            is_active: boolean;
+            name: string;
+          };
 
-					const newActiveGame = {
-						createdAt: event.created_at,
-						eventId: event.event_id,
-						id: event.id,
-						isActive: event.is_active,
-						name: event.name,
-					};
+          const newActiveGame = {
+            createdAt: event.created_at,
+            eventId: event.event_id,
+            id: event.id,
+            isActive: event.is_active,
+            name: event.name,
+          };
 
-					if (newActiveGame.isActive) {
-						setActiveGame(newActiveGame as ActiveGame);
-						// Fetch new drawn numbers for the new active game
-						const { data: newDrawnNumbers } = await supabase
-							.from("drawn_numbers")
-							.select("*")
-							.eq("game_id", newActiveGame.id);
+          if (newActiveGame.isActive) {
+            setActiveGame(newActiveGame as ActiveGame);
+            // Fetch new drawn numbers for the new active game
+            const { data: newDrawnNumbers } = await supabase
+              .from("drawn_numbers")
+              .select("*")
+              .eq("game_id", newActiveGame.id);
 
-						console.log("newDrawnNumbers", newDrawnNumbers);
-						setDrawnNumbers(newDrawnNumbers as DrawnNumber[]);
-					}
-				},
-			)
-			.subscribe();
+            console.log("newDrawnNumbers", newDrawnNumbers);
+            setDrawnNumbers(newDrawnNumbers as DrawnNumber[]);
+          }
+        }
+      )
+      .subscribe();
 
-		return () => {
-			supabase.removeChannel(channel);
-		};
-	}, []);
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, []);
 
-	if (!activeGame) return null;
+  if (!activeGame) return null;
 
-	const lastNumber = drawnNumbers[0];
-
-	return (
-		<div className="flex gap-4 flex-1 w-full max-md:flex-col">
-			<div className="w-full">
-				<LastNumber lastNumber={lastNumber} />
-				<DrawnNumbers drawnNumbers={drawnNumbers} gameName={activeGame.name} />
-			</div>
-			<RealtimeNumbers drawnNumbers={drawnNumbers} gameName={activeGame.name} />
-		</div>
-	);
+  return (
+    <div className="flex gap-4 flex-1 w-full max-md:flex-col">
+      <div className="w-full">
+        <DrawnNumbers drawnNumbers={drawnNumbers} gameName={activeGame.name} />
+      </div>
+      <RealtimeNumbers drawnNumbers={drawnNumbers} gameName={activeGame.name} />
+    </div>
+  );
 }
